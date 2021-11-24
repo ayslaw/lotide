a6ff09232735bfd32782d1eafa94ff66c3e1eafc
Ayoub F <94278725+ayslaw@users.noreply.github.com>
2021-11-24 17:55:51 -0500

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,34 +1,24 @@
-const eqArrays = function(arr1, arr2) {
-
-    let i = 0;
-    let answer = Boolean;
-
-    for (i = 0; i < arr1.length; i++) {
-
-      if (arr1.length === arr2.length) {
-        if (arr1[i] === arr2[i]) {
-          answer = true;
-        } else if (arr1[i] !== arr2[i]) {
-          answer = false;
-        }
-      } else {
-        answer = false
-      }
+let eqArrays = function(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
     }
-    return answer;
+  }
+  return true;
+};
 
-  };
-
-  const assertArraysEqual = function(actual, expected) {
-    // actual is eqArray function
-    if (actual, expected) {
-      console.log("PASS");
-    } else {
-      console.log("FAIL");
+let assertArraysEqual = function(arr) {
+  if (arr === false) {
+    console.log('fail');
+  } else {
+    if (arr === true) {
+      console.log('pass');
     }
-  };
+  }
+};
 
-  module.exports = assertArraysEqual; 
+
+
 
 assertArraysEqual((eqArrays)([1, 2, 3], [3, 2, 1])); // => false
 assertArraysEqual((eqArrays)([1, 2, 3], [1, 2, 3])); // => true
