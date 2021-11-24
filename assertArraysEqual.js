739bd2fecdbcb837203744fbba16809210be204f
ayslaw <ayoubf.chp@gmail.com>
2021-11-24 18:48:48 -0500

    assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,37 @@
-let eqArrays = function(a, b) {
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false;
+const eqArrays = function(arr1, arr2) {
+
+  let i = 0;
+  let answer = Boolean;
+
+  for (i = 0; i < arr1.length; i++) {
+
+    if (arr1.length === arr2.length) {
+      if (arr1[i] === arr2[i]) {
+        answer = true;
+      } else if (arr1[i] !== arr2[i]) {
+        answer = false;
+      }
+    } else {
+      answer = false
     }
   }
-  return true;
+  return answer;
+
 };
 
 let assertArraysEqual = function(arr) {
   if (arr === false) {
-    console.log('fail');
-  } else {
-    if (arr === true) {
       console.log('pass');
-    }
+  } else {
+      if (arr === true) {
+          console.log('fail');
+      }
   }
 };
 
-
-
+module.exports = assertArraysEqual; 
 
 assertArraysEqual((eqArrays)([1, 2, 3], [3, 2, 1])); // => false
 assertArraysEqual((eqArrays)([1, 2, 3], [1, 2, 3])); // => true
 assertArraysEqual((eqArrays)(["1", "2", "3"], ["1", "2", "3"])); // => true
-assertArraysEqual((eqArrays)(["1", "2", "3"], ["1", "2", 3])); // => falseo
+assertArraysEqual((eqArrays)(["1", "2", "3"], ["1", "2", 3])); // => false
