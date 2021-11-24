5cf976cf6f1805a5be796752178b3cbdfc481d8a
ayslaw <ayoubf.chp@gmail.com>
2021-11-24 18:08:02 -0500

    without.js

---
+++ without.js
@@ -0,0 +1,12 @@
+const without = function(source, seeya) {
+  let later = [];
+  let hello = [];
+  for (let i = 0; i < source.length; i++) {
+    if (seeya.includes(source[i])) {
+      later.push(source[i]);
+    } else {
+      hello.push(source[i]);
+    }
+  }
+  return hello;
+};
