b1cbc250111606a2a84a797aa480591e7f9c0311
ayslaw <ayoubf.chp@gmail.com>
2021-11-24 00:32:26 -0500

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else if (actual !== expected) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  let i = 0;
+  let answer = Boolean;
+
+  for (i = 0; i < arr1.length; i++) {
+
+    if (arr1.length === arr2.length) {
+      if (arr1[i] === arr2[i]) {
+        answer = true;
+      } else if (arr1[i] !== arr2[i]) {
+        answer = false;
+      }
+    } else {
+      answer = false
+    }
+  }
+  return answer;
+
+};
+
+module.exports = eqArrays;
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
