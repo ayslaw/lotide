301b6ad7e5f62ff98366546f0492bb6caf88ece9
ayslaw <ayoubf.chp@gmail.com>
2021-12-01 03:57:58 -0500

    refractored code

--- tail.js
+++ tail.js
@@ -1,18 +1,11 @@
+const assertEqual = require('./assertEqual');
+
 const tail = function(array) {
-  array = array.slice[1];
-  return (array);
+  console.log(array);
+  let tail = array.slice(1);
+  return tail;
 };
 
+tail(["Hello", "Lighthouse", "Labs"]);
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
