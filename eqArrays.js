ecafff4e0238c2f9f3f672b72bd79e3061a9f514
ayslaw <ayoubf.chp@gmail.com>
2021-12-01 03:59:21 -0500

    refractored code

--- eqArrays.js
+++ eqArrays.js
@@ -1,32 +1,19 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else if (actual !== expected) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
 
-  let i = 0;
-  let answer = Boolean;
-
-  for (i = 0; i < arr1.length; i++) {
-
-    if (arr1.length === arr2.length) {
-      if (arr1[i] === arr2[i]) {
-        answer = true;
-      } else if (arr1[i] !== arr2[i]) {
-        answer = false;
-      }
-    } else {
-      answer = false
+ 
+    if (arr1.length !== arr2.length) {
+        return false;
     }
-  }
-  return answer;
 
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+    return true;
+  
 };
-
-module.exports = eqArrays;
-
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+  module.exports = eqArrays;
